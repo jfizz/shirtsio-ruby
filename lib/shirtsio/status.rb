5506c4a94de769e213a058d8b2821a03fffa66f5
Dean Hailin Song <dean.song@movit-tech.com>
2013-05-16 15:29:09 +0800

    change order status API as the Shirts.io API changes

--- lib/shirtsio/status.rb
+++ lib/shirtsio/status.rb
@@ -4,10 +4,8 @@ module Shirtsio
     @status_url = 'status/'
 
     def Status.get_order_status(params={})
-      order_id = params[:order_id]
-      order_status_url = @status_url + order_id + '/'
-      response, api_key = Shirtsio.request(:get, order_status_url, @api_key, params={})
-      #Util.convert_to_shirtsio_object(response, api_key)
+      response, api_key = Shirtsio.request(:get, @status_url, @api_key, params)
+      Util.convert_to_shirtsio_object(response, api_key)
       response
     end
 
